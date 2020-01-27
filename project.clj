b610514c6fc3f5232972ac6df5b28066e3530327
Diego Escala <diego.escala@sentientreality.com>
2020-01-27 10:26:04 -0600

    Disabled release signing

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject validate-credit-card "1.0.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/diegoescala/validate-credit-card"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns validate-credit-card.core})
+  :repl-options {:init-ns validate-credit-card.core}
+  :deploy-repositories [[:clojars {:url "[https://clojars.org/](https://clojars.org/)" 
+                                   :sign-releases false}]])
