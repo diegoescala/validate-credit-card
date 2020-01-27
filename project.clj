672c93ded7b369b46af76e2d28fb3fd0e5a41dbe
Diego Escala <diego.escala@sentientreality.com>
2020-01-27 10:55:12 -0600

    Updated project def

--- project.clj
+++ project.clj
@@ -5,6 +5,17 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns validate-credit-card.core}
+
+  :repositories [["java.net" "https://download.java.net/maven/2"]
+                 ["clojars" {:url "https://clojars.org/repo"
+                             :snapshots false
+                             :sign-releases false
+                             :checksum :fail
+                             :update :always
+                             :releases {:checksum :fail :update :always}}]
+
+                 ["snapshots" "https://clojars.org/repo"]
+                 ["releases" {:url "https://clojars.org/repo"}]]
   :deploy-repositories [["releases"  {:sign-releases false
                                       :url "https://clojars.org/repo"}]
                         ["snapshots" {:sign-releases false
